e4a3e7ebbb142126f0657e7c3f2dbd0880598b3f
Aditya Gupta <guptaaditya.13@gmail.com>
2015-06-17 19:13:52 +0530

    Building login model

--- Model/Models.php
+++ Model/Models.php
@@ -5,33 +5,56 @@ require('../connection.php')
 */
 class Login
 {
+	/**
+	 *@return array.
+	 * This method returns all the data present in the login table.
+	*/
 	public function getAll()
 	{
 		$sql = "SELECT * FROM login_table";
 		$conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
 		$result = mysqli_query($sql,$conn);
+		mysqli_close($conn);
 		return $result;
 	}
-
+	/**
+	 *@return array
+	 * This method returns an array containing the object with id = $id 
+	*/
 	public function get($id)
 	{
 		$sql = "SELECT * FROM login_table WHERE id = $id";
 		$conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
 		$result = mysqli_query($sql,$conn);
+		mysqli_close($conn);
 		return $result;	
 	}
+	/**
+	 *@return string
+	 * This method checks whether the username is matched the regex or not. 
+	*/
 	public function validateData($username, $password){
-
+		$error = null;
+		if (!preg_match('/^[a-zA-Z0-9.]*$/', $username)) {
+			$error = "username of invalid pattern";
+		}
+		return $error;
+	}
+	/**
+	 *@return array containg user information
+	 * This method is used to get the user information from its username and password.
+	*/
+	public function getUser($username, $password){
+		$sql = "SELECT * FROM login_table WHERE username = $username AND password = $password";
+		$conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
+		$result = mysqli_query($sql,$conn);
+		if (mysqli_num_rows($result) == 1){
+			mysqli_close($conn);
+			return $result;
+		} else {
+			mysqli_close($conn);
+			return null;
+		}
 	}
 }
-
-
-
-
-
-
-
-
-
-
 ?>
