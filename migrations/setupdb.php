2aa5ae526c91f944ae94ab6c801b51d2c26cf56e
Sunny Narayan <sunny.cs13@iitp.ac.in>
2015-06-21 13:19:13 +0530

    code for creating the question table

--- migrations/setupdb.php
+++ migrations/setupdb.php
@@ -6,6 +6,9 @@
 require('../connection.php');
 $conn = mysqli_connect(SERVER_ADDRESS,USER_NAME,PASSWORD,DATABASE);
 
+/**
+ * Generating user_table
+ */
 $sql = "CREATE TABLE IF NOT EXISTS `user_table` (`id` int(11) NOT NULL,`username` varchar(40) NOT NULL,`password` char(40) DEFAULT NULL,`token` char(40) DEFAULT NULL,`cookie` char(40) DEFAULT NULL,`last_login` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,`email` varchar(100) NOT NULL,`name` varchar(100) NOT NULL,`total_logins` int(11) NOT NULL DEFAULT '0')";
 mysqli_query($conn,$sql);
 $sql = "ALTER TABLE `user_table` ADD PRIMARY KEY (`id`) COMMENT 'User id for the database';";
@@ -13,6 +16,27 @@ mysqli_query($conn,$sql);
 $sql = "ALTER TABLE `user_table` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
 mysqli_query($conn,$sql);
 $sql = "ALTER TABLE `user_table` ADD `type` INT(1)0 NOT NULL DEFAULT '0' COMMENT 'stores the type of user' ;";
+mysqli_query($conn,$sql);
+
+/**
+ * Generating question_table
+ */
+$sql = "CREATE TABLE IF NOT EXISTS `questions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `q_text` text DEFAULT NULL,
+  `q_image` varchar(20) DEFAULT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `max_marks` int(11) NOT NULL,
+  `sample_inp` text NOT NULL,
+  `sample_out` text NOT NULL,
+  `timestamps` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+mysqli_query($conn,$sql);
+$sql = "ALTER TABLE `questions` ADD PRIMARY KEY (`id`) COMMENT 'User id for the database';";
+mysqli_query($conn,$sql);
+$sql = "ALTER TABLE `user_table` MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;";
 mysqli_query($conn,$sql);
 mysqli_close($conn);
 ?>
